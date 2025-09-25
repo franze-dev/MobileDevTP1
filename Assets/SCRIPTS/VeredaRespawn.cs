9fdcab78c1c43c9f25dcb29459ab59c65d6f7de5
sofi <genuinedistinctclearcapital@gmail.com>
2025-09-25 15:56:28 -0300

    add logos for splash screen

--- Assets/SCRIPTS/VeredaRespawn.cs
+++ Assets/SCRIPTS/VeredaRespawn.cs
@@ -5,16 +5,9 @@ public class VeredaRespawn : MonoBehaviour
 {
 	public string PlayerTag = "Player";
 
-	// Use this for initialization
 	void Start () 
 	{
 		GetComponent<Renderer>().enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
 	}
 	
 	void OnTriggerEnter(Collider other)
