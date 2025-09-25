31128422f27b634b50bd3618348a95e1cd64657e
sofi <genuinedistinctclearcapital@gmail.com>
2025-09-25 17:40:07 -0300

    add serviceprovider

---
+++ Assets/SCRIPTS/Escenas/ProveedorServicios.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public static class ProveedorServicios
+{
+    private static readonly Dictionary<Type, object> Servicios = new();
+
+    public static void DefinirServicio(Type tipo, object servicio,
+                                  bool sobreescribir = false)
+    {
+        if (!Servicios.TryAdd(tipo, servicio) && sobreescribir)
+            Servicios[tipo] = servicio;
+    }
+
+    public static void DefinirServicio<T>(T servicio, bool sobreescribir = false)
+    {
+        if (!Servicios.TryAdd(typeof(T), servicio) && sobreescribir)
+            Servicios[typeof(T)] = servicio;
+    }
+    
+    public static object ObtenerServicio(Type tipo)
+    {
+        return Servicios.GetValueOrDefault(tipo);
+    }
+
+    public static bool IntentarObtenerServicio<T>(out T servicio) where T : class
+    {
+        if (Servicios.TryGetValue(typeof(T), out var miServicio)
+            && miServicio is T tServicio)
+        {
+            servicio = tServicio;
+            return true;
+        }
+
+        servicio = null;
+        return false;
+    }
+}
