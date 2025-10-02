e9f45c32061cb80ed82d1d4be82b9317f9a66d13
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-01 23:09:11 -0300

    fix final points canvas scaling

--- Assets/SCRIPTS/DetectorInput.cs
+++ Assets/SCRIPTS/DetectorInput.cs
@@ -8,12 +8,17 @@ public class DetectorInput : MonoBehaviour
         Touch
     }
 
-    public TipoInput InputAct;
+    public TipoInput InputAct = TipoInput.Teclado;
     public DetectorGestos DetectorGestos;
 
     private void Awake()
     {
         if (!DetectorGestos)
             DetectorGestos = GetComponent<DetectorGestos>();
+
+#if UNITY_ANDROID || UNITY_IPHONE
+        InputAct = TipoInput.Touch;
+#endif
+
     }
 }
